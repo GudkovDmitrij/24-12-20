6dc13737062d9d22f6484a5bd0ce232b290d8db8
Ivan <example@gmail.com>
2020-12-22 09:53:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log(4 + "5")
+
+let incr = 10,
+decro = 10;
+
+//++incr;
+//--decro;
+
+console.log(++incr);
+console.log(--decro);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const isCheked = true,
+    isClose = true;
+
+    console.log(isCheked && isClose);
